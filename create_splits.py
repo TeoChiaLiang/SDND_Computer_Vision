01f97ceadb4c0660701e9c940a27a9cd618f1e91
ChiaLiang <teo.chialiang@stengg.com>
2022-01-01 20:42:16 +0800

    Implementation of dataset split

--- create_splits.py
+++ create_splits.py
@@ -6,6 +6,7 @@ import random
 import numpy as np
 
 from utils import get_module_logger
+from pathlib import Path
 
 
 def split(source, destination):
@@ -18,6 +19,44 @@ def split(source, destination):
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
     # TODO: Implement function
+
+    if not os.path.exists(source + "/train"):
+        os.makedirs(source + "/train")
+
+    if not os.path.exists(source + "/val"):
+        os.makedirs(source + "/val")
+
+    if not os.path.exists(source + "/test"):
+        os.makedirs(source + "/test")
+
+    result = glob.glob(source + "/*.tfrecord")
+
+    random.shuffle(result)
+
+    resultSize = len(result)
+    startIdx = int(0.0*resultSize)
+    endIdx = int(0.8*resultSize)
+    print("train: ", startIdx," - ", endIdx)
+    result_train = result[startIdx : endIdx]
+    for path in result_train:
+        lStr = path.split("/")
+        os.symlink(path, source + "/train/" + lStr[-1])  
+
+    startIdx = int(0.8*resultSize)
+    endIdx = int(0.9*resultSize)
+    print("val: ", startIdx," - ", endIdx)
+    result_val = result[startIdx : endIdx]
+    for path in result_val:
+        lStr = path.split("/")
+        os.symlink(path, source + "/val/" + lStr[-1])  
+
+    startIdx = int(0.9*resultSize)
+    endIdx = int(1.0*resultSize)
+    print("test: ", startIdx," - ", endIdx)
+    result_test = result[startIdx : endIdx]
+    for path in result_test:
+        lStr = path.split("/")
+        os.symlink(path, source + "/test/" + lStr[-1])  
 
 
 if __name__ == "__main__":
